b55bcd7ee89b9eb4d5589b41ba9ce50e0eec1c0a
pollo <pollosatyi@mail.ru>
2025-09-22 22:13:45 +0300

    маппинг

--- WeatherTelegramBot/Profiles/WeatherProfile.cs
+++ WeatherTelegramBot/Profiles/WeatherProfile.cs
@@ -3,6 +3,7 @@ using WeatherTelegramBot.DTOs;
 using WeatherTelegramBot.Models;
 using static WeatherTelegramBot.DTOs.CreateWeatherDto;
 
+
 namespace WeatherTelegramBot.Profiles
 {
     public class WeatherProfile : Profile
@@ -10,14 +11,18 @@ namespace WeatherTelegramBot.Profiles
         public WeatherProfile()
         {
 
-
-
-
-            CreateMap<CreateWeatherDto, WeatherModel>()
+            //Маппинг из сложного WeatherDto в простую WeatherModel
+            CreateMap<WeatherDto, WeatherModel>()
                 .ForMember(dest => dest.City, opt => opt.MapFrom(src => src.Name))
                 .ForMember(dest => dest.Temperature, opt => opt.MapFrom(src => src.Main.Temp))
                 .ForMember(dest => dest.Description, opt => opt.MapFrom(src => src.Weather.First().Description))
                 .ForMember(dest => dest.WindSpeed, opt => opt.MapFrom(src => src.Wind.Speed));
+
+            // Использование
+
+
+            CreateMap<CreateWeatherDto, WeatherModel>();
+
 
         }
     }
